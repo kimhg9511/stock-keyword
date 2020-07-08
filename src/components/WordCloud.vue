64922bf158260a24d71bcb3b069c69abce7502ff
kimhg <kimhgedu@users.noreply.github.com>
2020-07-08 18:47:29 +0900

    [2020-07-08] update (word cloud)

--- src/components/WordCloud.vue
+++ src/components/WordCloud.vue
@@ -5,53 +5,32 @@
 <script>
 import * as d3 from "d3";
 import * as cloud from "d3-cloud";
+import * as keywords from "../../python_ws/rtop20_stocks20200707.json"
 export default {
   name: 'app',
   data() {
     return { 
-      fruits: [
-        {'name' : '사과',  'count': 3},
-        {'name' : '바나나', 'count': 2},
-        {'name' : '포도',  'count': 1},
-        {'name' : '키위',   'count': 3},
-        {'name' : '메론',  'count': 2},
-        {'name' : '오렌지',  'count': 1},
-        {'name' : '수박', 'count': 3},
-        {'name' : '귤',  'count': 2},
-        {'name' : '과일',   'count': 1},
-        {'name' : '참외',  'count': 3},
-        {'name' : '복숭아',  'count': 2},
-        {'name' : 'ㅎㅎ', 'count': 1},
-        {'name' : '겹침',  'count': 3},
-        {'name' : '해결이', 'count': 2},
-        {'name' : '안됨',  'count': 1},
-        {'name' : '사과',  'count': 3},
-        {'name' : '바나나', 'count': 2},
-        {'name' : '포도',  'count': 1},
-        {'name' : '키위',   'count': 3},
-        {'name' : '메론',  'count': 2},
-        {'name' : '오렌지',  'count': 1},
-        {'name' : '수박', 'count': 3},
-        {'name' : '귤',  'count': 2},
-        {'name' : '과일',   'count': 1},
-        {'name' : '참외',  'count': 3},
-        {'name' : '복숭아',  'count': 2},
-        {'name' : 'ㅎㅎ', 'count': 1},
-        {'name' : '겹침',  'count': 3},
-        {'name' : '해결이', 'count': 2},
-        {'name' : '안됨',  'count': 1},
-      ]
+      keywords : keywords.default,
     }
   },
   mounted() {
-    this.generateCloud();
+    this.generateCloud(this.keywords, 'archimedean');
+    this.generateCloud(this.keywords, 'rectangular');
   },
   methods: {
-    generateCloud(){
-      let width = 450;
-      let height = 450;
-      let fontSize = 14;
-      let fruits = this.fruits;
+    generateCloud(keywords, spiral){
+      let mainKeywords = Object.keys(keywords);
+      let counts = mainKeywords.map(elem => keywords[elem]['count']);
+      let cMax = counts.reduce((a,b) => a > b ? a : b);
+      let cMin = counts.reduce((a,b) => a > b ? b : a);
+      let fontSize = 20;
+      let fontMax = 2;
+      let fontMin = 1;
+      const fontSizeScale = d3.scaleLinear()
+                            .domain([cMin, cMax])
+                            .range([fontMin, fontMax])
+      let width = 800;
+      let height = 300;
       let colors = ["#111","#333","#555","#777","#999"]
       let svg = d3.select("#text-cloud").append("svg")
           .attr('width', width)
@@ -59,35 +38,48 @@ export default {
           
       let layout = cloud()
         .size([width, height])
-        .words(fruits.map(function(d) { return {text: d['name']}; }))
+        .words(mainKeywords.map(function(d) { return {text: d}; }))
         .padding(0)
         .rotate(0)
         .text(d => d.text)        
         .font('Impact')     
-        .fontSize(fontSize*3)
+        .fontSize(fontSize* fontMax)
+        .spiral(spiral)
         .on("end", draw);
       layout.start();
+      const generateBox = this.generateBox;
 
       function draw(words) {
         svg.append("g")
           .attr("transform",`translate(${layout.size()[0]/2},${layout.size()[1]/2})`)
           .selectAll("text")
           .data(words)
-          .enter().append("text")
+          .enter()
+          .append("text")
+            .text(d => d.text)
             .style("font-family", "Impact")
             .style("fill", (d,i) => colors[i%colors.length])
             .attr("text-anchor", "middle")
             .style("cursor", "pointer")
             .style("font-weight", "bold")
-            .style("font-size", (d, i) => {
-              return fontSize * fruits[i]['count'];
+            .style("font-size", d => {
+              return fontSize * fontSizeScale(keywords[d.text]['count']);
             })
+            .on('click', d => {
+              console.dir(d3.select(this));
+              generateBox(keywords[d.text]['count']);
+            })
+            .transition()
+            .duration(600)
+            .delay((d, i) => i * 20)
             .attr("transform", d => 
               `translate(${[d.x, d.y]})rotate(${d.rotate})`
-            )
-            .text(d => d.text)
-
+            );
       }
+    },
+    generateBox(text){
+      console.log(text)
+      console.log(this);
     },
   }
 }
