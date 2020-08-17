edf5ee0a262c299b2246468ae84d4835ebabcff0
kimhg <kimhgedu@users.noreply.github.com>
2020-08-17 17:31:38 +0900

    [2020-08-17] 실시간 급상승

--- src/components/WordNetwork.vue
+++ src/components/WordNetwork.vue
@@ -4,7 +4,7 @@
 </template>
 
 <script>
-import * as d3 from "d3";
+// import * as d3 from "d3";
 import * as keywords from "../../python_ws/rtop20_stocks20200707.json"
 export default {
   name: 'app',
@@ -17,23 +17,23 @@ export default {
     this.generateWordNet()
   },
   methods: {
-    generateWordNet(){
-      let svgWidth = 500;
-      let svgHeight = 500;
-      let keywords = this.keywords;
-      console.log(keywords)
-      const svg = d3.select('#svg')
-                      .append('svg')
-                      .attr('width', svgWidth)
-                      .attr('height', svgHeight)
-                      .style('background-color', '#000')
-      const g = svg.append('g')
-          .selectAll('text')
-          .data(keywords)
-          .enter()
-      const g2 = svg.append('g')
-    }
-  },
+    // generateWordNet(){
+    //   let svgWidth = 500;
+    //   let svgHeight = 500;
+    //   let keywords = this.keywords;
+    //   console.log(keywords)
+    //   const svg = d3.select('#svg')
+    //                   .append('svg')
+    //                   .attr('width', svgWidth)
+    //                   .attr('height', svgHeight)
+    //                   .style('background-color', '#000')
+    //   const g = svg.append('g')
+    //       .selectAll('text')
+    //       .data(keywords)
+    //       .enter()
+    //   const g2 = svg.append('g')
+    // }
+  }
 }
 </script>
 
